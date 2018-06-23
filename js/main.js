b577f6484fa79f4cb8444e3a9f8ba224f8058ad7
caoglish <caoglish.email@gmail.com>
2018-06-24 01:39:55 +1000

    1. add resetData methods. 2. improve the code

--- js/main.js
+++ js/main.js
@@ -1,23 +1,7 @@
 (function (window) {
-    let orginalData = _(_.range(1, 100)).shuffle().shuffle().slice(0, 10).value();
-    let millisecond = 300;
- 
-    let bublesort = function (array, record) {
-        for (var i = 0; i < array.length; i++) {
-            for (var j = 0; j < array.length - i - 1; j++) {
-                record.bind(this)(array);
-                if (array[j] > array[j + 1]) {
-                    var tmp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = tmp;
-                }
-            }
-        }
-        record.bind(this)(array);
-    }
 
+    let millisecond = 500;
 
-  
     let setTimeoutPromise = function (func, ms) {
         ms = ms || 0;
         return new Promise(
@@ -29,34 +13,42 @@
         )
     }
 
- 
-    var queue={
+    var queue = {
         queue: Promise.resolve(),
-        add: function(func) {
-           this.queue = this.queue.then(() => {
-                return setTimeoutPromise( ()=> {
+        add: function (func) {
+            this.queue = this.queue.then(() => {
+                return setTimeoutPromise(() => {
                     func.bind(this)();
                 }, millisecond)
             })
         }
     };
- 
 
-    
-   
+    let bublesort = function (array, record) {
 
-  
+        for (var i = 0; i < array.length; i++) {
+            for (var j = 0; j < array.length - i - 1; j++) {
 
+                if (array[j] > array[j + 1]) {
+                    var tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                    record.bind(this)(array);
+                }
+            }
+        }
+        record.bind(this)(array);
+    }
 
-    let insertsort = function (array,record) {
+    let insertsort = function (array, record) {
         for (var i = 1; i < array.length; i++) {
             for (var j = i; j - 1 >= 0; j--) {
-                record(array)
+
                 if (array[j] < array[j - 1]) {
                     var temp = array[j];
                     array[j] = array[j - 1];
                     array[j - 1] = temp;
-                    
+                    record(array)
                 }
 
             }
@@ -68,7 +60,7 @@
         for (var i = 0; i < array.length; i++) {
             var min = i;
             for (var j = i + 1; j < array.length; j++) {
-                record(array)
+
                 if (array[j] < array[min]) {
                     min = j;
                 }
@@ -76,13 +68,19 @@
             var temp = array[i];
             array[i] = array[min];
             array[min] = temp;
-            
+            record(array)
         }
         record(array)
     };
 
+    let makeRandomNum = () => {
+        return _(_.range(1, 100)).shuffle().shuffle().slice(0, 10).value();
+    };
 
-    sortList={
+    initLines = makeRandomNum();
+
+
+    sortList = {
         bublesort: bublesort,
         insertsort: insertsort,
         selectionsort: selectionsort
@@ -93,18 +91,31 @@
         el: "#app",
         data: {
             message: "Vuejs is work",
-            lines: orginalData,
-            originalLines: _.clone(orginalData),
-            isStarted: false
+            lines: initLines,
+            originalLines: _.clone(initLines),
+            isShowApp: true,
+            isDone: true
         },
         created: function () {},
         methods: {
+            resetData() {
+                this.isShowApp = false;
+                this.lines = makeRandomNum();
+                this.originalLines = _.clone(this.lines);
+                queue.add(() => (this.isShowApp = true));
+            },
             sort: function () {
-                sort(this.lines, (list)=> {
-                    let tmplist=_.clone(list)
-                    queue.add(()=>{
+                this.isDone = false;
+                this.reset()
+
+                sort(_.clone(this.lines), (list) => {
+                    let tmplist = _.clone(list)
+                    queue.add(() => {
                         this.lines = _.clone(tmplist);
                     });
+                });
+                queue.add(() => {
+                    this.isDone = true;
                 });
             },
             reset() {
