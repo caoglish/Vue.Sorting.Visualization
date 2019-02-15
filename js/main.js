d10bf7efcfa14b07ad3527e4f3ae1f6efca5cce0
caoglish <caoglish.email@gmail.com>
2019-02-15 14:51:48 +1100

    1. make visual sort component. 2. make 3 visual sort component each will run one type of sort Algorithm 3. update queue module so each component will have its own queue. 4. global reset data and sort all function

--- js/main.js
+++ js/main.js
@@ -1,43 +1,43 @@
 (function (window) {
+	let millisecond = 300;
+	let sortComponentList=[];
 
-    let millisecond = 500;
+	let setTimeoutPromise = function (func, ms) {
+		ms = ms || 0;
+		return new Promise(
+			function (resolve, reject) {
+				setTimeout(() => {
+					resolve(func());
+				}, ms);
+			}
+		)
+	}
 
-    let setTimeoutPromise = function (func, ms) {
-        ms = ms || 0;
-        return new Promise(
-            function (resolve, reject) {
-                setTimeout(() => {
-                    resolve(func());
-                }, ms);
-            }
-        )
-    }
-
-    var queue = {
-        queue: Promise.resolve(),
-        add: function (func) {
-            this.queue = this.queue.then(() => {
-                return setTimeoutPromise(() => {
-                    func.bind(this)();
-                }, millisecond)
-            })
-        }
-    };
+	var queueGenerator = function () {
+		return {
+			queue: Promise.resolve(),
+			add: function (func) {
+				this.queue = this.queue.then(() => {
+					return setTimeoutPromise(() => {
+						func.bind(this)();
+					}, millisecond)
+				})
+			}
+		};
+	}
 
     let bublesort = function (array, record) {
-
-        for (var i = 0; i < array.length; i++) {
+		for (var i = 0; i < array.length; i++) {
             for (var j = 0; j < array.length - i - 1; j++) {
-
-                if (array[j] > array[j + 1]) {
+		        if (array[j] > array[j + 1]) {
                     var tmp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = tmp;
-                    record.bind(this)(array);
+                    record(array);
                 }
             }
         }
-        record.bind(this)(array);
+        record(array);
     }
 
     let insertsort = function (array, record) {
@@ -56,65 +56,70 @@
         record(array)
     };
 
-    let selectionsort = function (array, record) {
-        for (var i = 0; i < array.length; i++) {
-            var min = i;
-            for (var j = i + 1; j < array.length; j++) {
+	let selectionsort = function (array, record) {
+		for (var i = 0; i < array.length; i++) {
+			var min = i;
+			for (var j = i + 1; j < array.length; j++) {
 
-                if (array[j] < array[min]) {
-                    min = j;
-                }
-            }
-            var temp = array[i];
-            array[i] = array[min];
-            array[min] = temp;
-            record(array)
-        }
-        record(array)
-    };
+				if (array[j] < array[min]) {
+					min = j;
+				}
+			}
+			var temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			record(array)
+		}
+		record(array)
+	};
 
     let makeRandomNum = () => {
         return _(_.range(1, 100)).shuffle().shuffle().slice(0, 10).value();
     };
-
     initLines = makeRandomNum();
 
-
-    sortList = {
+    let sortList = {
         bublesort: bublesort,
         insertsort: insertsort,
         selectionsort: selectionsort
-    }
-    let sort = bublesort;
+	}
 
-    window.app = new Vue({
-        el: "#app",
-        data: {
-            message: "Vuejs is work",
+	
+	window.dev=sortComponentList
+	Vue.component('visual-sort',{
+		template:'#visual-sort-template',
+		props: ['type'],
+        data:function() {
+			return {
+			queue:queueGenerator(),
+            message: "Vuejs is work incomponent",
             lines: initLines,
             originalLines: _.clone(initLines),
             isShowApp: true,
-            isDone: true
+			isDone: true,
+			sortAlgorithm:sortList[this.type]
+			}
         },
-        created: function () {},
+        created: function () {
+			sortComponentList.push(this);
+		},
         methods: {
-            resetData() {
+            resetData(data) {
                 this.isShowApp = false;
-                this.lines = makeRandomNum();
+                this.lines = data;
                 this.originalLines = _.clone(this.lines);
-                queue.add(() => (this.isShowApp = true));
+                this.queue.add(() => (this.isShowApp = true));
             },
             sort: function () {
                 this.isDone = false;
                 this.reset()
-
-                sort(_.clone(this.lines), (list) => {
+                this.sortAlgorithm(_.clone(this.lines), (list) => {                        
                     let tmplist = _.clone(list)
-                    queue.add(() => {
+                    this.queue.add(() => {
                         this.lines = _.clone(tmplist);
                     });
                 });
-                queue.add(() => {
+                this.queue.add(() => {
                     this.isDone = true;
                 });
             },
@@ -122,9 +127,42 @@
                 this.lines = _.clone(this.originalLines);
             },
             setSortAlgorithm(algorithm) {
-                sort = sortList[algorithm];
+                this.sortAlgorithm = sortList[algorithm];
             },
         }
+    })
+
+    window.app = new Vue({
+		el: "#app",
+		component:["visual-sort"],
+		data:{
+			sortComponentList:sortComponentList,
+			lines: initLines,
+            originalLines: _.clone(initLines),
+		},
+		methods: {
+			resetData() {
+                this.lines = makeRandomNum();
+                this.originalLines = _.clone(this.lines);
+				_.each(this.sortComponentList,(sc)=>{
+					sc.resetData(this.originalLines);
+				});
+
+			},
+			sort(){
+				_.each(this.sortComponentList,(sc)=>{
+					sc.sort();
+				});
+			}
+		},
+		computed:{
+			isDone: function () {
+				return this.sortComponentList.reduce((sum,sc)=>{
+					return sum&sc.isDone;
+				},true)
+			  }
+
+		}
     });
 
 })(window);
